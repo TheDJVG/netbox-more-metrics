ac0e01dfa1c36d8d1ab888aee924dd0eddd9c267
Daan van Gorkum <djvg@djvg.net>
2024-07-02 22:03:34 +0800

    Revert "Merge pull request #20 from nikolasj/development"
    This reverts commit 13c5cacf35de3ce00206bdef50544105bce34d81, reversing
    changes made to ddbaa8036a8042c10125f906ce161c939697cfd8.
    
    get_utilization is not actually supported on ipam.IPRange

--- netbox_more_metrics/choices.py
+++ netbox_more_metrics/choices.py
@@ -38,7 +38,6 @@ class MetricValueChoices(ChoiceSet):
             "ipam.Aggregate",
             (("get_utilization", "Percentage Utilized"),),
         ),
-        ("ipam.Iprange", (("get_utilization", "Percentage Utilized"),)),
     )
     CHOICES_BY_MODEL = dict(CHOICES)
 
